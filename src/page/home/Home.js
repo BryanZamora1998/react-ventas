6104c83f4f821144a1ced111116a54c4fba669a7
Bryan Zamora <bszamora@pichincha.com>
2022-01-10 19:07:09 -0500

    diseño de menu parte 2 https://www.youtube.com/watch?v=O9l75KOB2pE

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -3,17 +3,14 @@ import {
 
   BrowserRouter as Router,
   Switch,
-  useRouteMatch,
-  Redirect,
-  Link,
-  Route
+  useRouteMatch
   
 } from "react-router-dom";
 import './Home.css';
 import Seguridad from "./seguridad/Seguridad";
 import RoutePrivate from "../../util/routePrivate/RoutePrivate";
 import { useHistory } from "react-router-dom";
-import Login from "../login/Login";
+import $ from 'jquery';
 
 export default function App() {
   const history = useHistory();
@@ -25,36 +22,57 @@ export default function App() {
     history.push("../login");
   }
 
-  let match = useRouteMatch();
+  const clickMe = (texto) => (event) => {
 
-    return (
-      <Router>
-          
-          <nav className="sidebar">
-            <div className="text">Side menu</div>
-              <ul>
-                <li><a href="#">Dasboard</a></li>
+    if($(`#${texto}-show`).hasClass("show")){
+
+      console.log("Eliminado");
+      $(`#${texto}-show`)
+      .removeClass("show")
+      .addClass("unShow");
+
+      $(`#${texto}`)
+      .removeClass("active");
+
+    }else{
+      
+      console.log("guardado");
+      $(`#${texto}-show`)
+      .removeClass("unShow")
+      .addClass("show")
+      .addClass("active");
+
+      $(`#${texto}`)
+      .addClass("active");
+    }
+  }
+
+    let match = useRouteMatch();
+
+      return (
+        <Router>
+            
+            <nav className="sidebar">
+              <div className="text">Side menu</div>
+                <ul>
+                  <li><a href="#">Dasboard</a></li>
                 <li>
-                  <a href="#">Feacture
-                    <span className="fas fa-caret-down"></span>
-                  </a>
-                   <ul>
+                   <a href="#" id="feat" onClick={clickMe("feat")}>Feedback <span className="fa fa-caret-down first"></span></a>
+                   <ul id="feat-show" className="unShow">
                      <li><a href="#">Page</a></li>
                      <li><a href="#">Elements</a></li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="#">Service
-                  <span className="fas fa-caret-down"></span>
-                  </a>
-                   <ul>
-                     <li><a href="#">Disagner</a></li>
-                     <li><a href="#">web designe</a></li>
                   </ul>
                 </li>
                  <li><a href="#">Portafolio</a></li>
                  <li><a href="#">Overview</a></li>
                  <li><a href="#">Shortcuts</a></li>
+                 <li>
+                   <a href="#" id="serv" onClick={clickMe("serv")}>Servicios <span className="fa fa-caret-down first serv"></span></a>
+                   <ul id="serv-show" className="unShow">
+                     <li><a href="#">Page</a></li>
+                     <li><a href="#">Elements</a></li>
+                  </ul>
+                </li>
                  <li><a href="#">Feedback</a></li>
               </ul>
           </nav>
